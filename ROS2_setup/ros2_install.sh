d22ac93107be00abba3553b9cfcfc4aec961fdb4
s40523122 <40523122@gm.nfu.edu.tw>
2021-07-14 21:26:31 +0800

    add ros2_setup

---
+++ ROS2_setup/ros2_install.sh
@@ -0,0 +1,16 @@
+# Setup Sources
+sudo apt update 
+sudo apt install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Installation
+sudo apt update
+sudo apt install -y ros-foxy-ros-base
+
+# Environment setup
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Dependencies for building packages
